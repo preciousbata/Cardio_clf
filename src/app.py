fe2179d210f1dbc2d8224f2e44713bba72468af4
Precious Batta <precyb007@gmail.com>
2022-03-21 03:32:52 +0100

    Update app.py

--- src/app.py
+++ src/app.py
@@ -7,7 +7,7 @@ import pickle
 
 
 app = Flask(__name__, template_folder='../template', static_folder='../static')
-model = pickle.load(open('./models/clf_model.pkl', 'rb'))
+model = pickle.load(open('../models/clf_model.pkl', 'rb'))
 # model = pickle.load(open(config.LOAD_MODEL_2, 'rb'))
 
 
